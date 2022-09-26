6ad884baf8a4966d1f82d44b5518ab86f29b5d77
chungjung <chungjung@DESKTOP-AH2B8FO>
2022-09-26 11:09:28 +0900

    kustomize

---
+++ ubuntu/kustomize.install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+## INFO: https://kubectl.docs.kubernetes.io/installation/kustomize/binaries/
+
+set -euf -o pipefail
+
+# Download kustomize binary
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+
+# Install to /usr/local/bin
+sudo install -o root -g root -m 0755 kustomize /usr/local/bin/kustomize
