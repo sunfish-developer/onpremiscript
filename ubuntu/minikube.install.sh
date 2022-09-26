d7096fd037cd61a54715de7adc99b2679c3140ea
chungjung <chungjung@DESKTOP-AH2B8FO>
2022-09-26 11:18:53 +0900

    minikube

---
+++ ubuntu/minikube.install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+## INFO: https://minikube.sigs.k8s.io/docs/start/
+
+
+set -euf -o pipefail
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
